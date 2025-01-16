ded7dac064a13259df7da061575eaae309da6bdc
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2025-01-16 08:30:01 -0500

    feat: add solucion ejercicio 2

--- CLASE02/Empleado/src/PruebaEmployee.java
+++ CLASE02/Empleado/src/PruebaEmployee.java
@@ -2,6 +2,7 @@ public class PruebaEmployee {
     public static void main(String[] args) throws Exception {
         Employee empleado1 = new Employee("Jeferson", 12221, 2300000);
         Employee empleado2 = new Employee("Ana", 12222, 1300000);
+        Manager gerente1 = new Manager("Mary", 12223, 2400000, "Dev");
         System.out.println(empleado1.toString());
 
         empleado1.increaseSalary(12);
@@ -15,5 +16,7 @@ public class PruebaEmployee {
         System.out.println(empleado1.hashCode());
 
         System.out.println(empleado2.hashCode());
+
+        System.out.println(empleado1.equals(empleado2));
     }
 }
